cb725a406e7ec9d5577c95b9ab1ad5c9c783c027
Rand Kmiec <rkmiec@globusmedical.com>
2024-10-30 09:25:45 -0400

    Disabled X/Y/Z idle default

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1248,9 +1248,9 @@
  * The default timeout duration can be overridden with M18 and M84. Set to 0 for No Timeout.
  */
 #define DEFAULT_STEPPER_TIMEOUT_SEC 0
-//#define DISABLE_IDLE_X
-//#define DISABLE_IDLE_Y
-//#define DISABLE_IDLE_Z    // Disable if the nozzle could fall onto your printed part!
+#define DISABLE_IDLE_X
+#define DISABLE_IDLE_Y
+#define DISABLE_IDLE_Z    // Disable if the nozzle could fall onto your printed part!
 //#define DISABLE_IDLE_I
 //#define DISABLE_IDLE_J
 //#define DISABLE_IDLE_K
