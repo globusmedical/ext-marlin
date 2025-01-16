51bb6dd1e65f3bfa2b487f67751bd851f9f6846d
Rand Kmiec <rkmiec@globusmedical.com>
2025-01-16 09:21:46 -0500

    Added smaller minimum steps so that resolution of angular stages is closer to 0.03 degrees than 0.18 degrees, version 1.02.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1248,9 +1248,9 @@
  * The default timeout duration can be overridden with M18 and M84. Set to 0 for No Timeout.
  */
 #define DEFAULT_STEPPER_TIMEOUT_SEC 0
-#define DISABLE_IDLE_X
-#define DISABLE_IDLE_Y
-#define DISABLE_IDLE_Z    // Disable if the nozzle could fall onto your printed part!
+//#define DISABLE_IDLE_X
+//#define DISABLE_IDLE_Y
+//#define DISABLE_IDLE_Z    // Disable if the nozzle could fall onto your printed part!
 //#define DISABLE_IDLE_I
 //#define DISABLE_IDLE_J
 //#define DISABLE_IDLE_K
@@ -2558,7 +2558,7 @@
 // @section motion
 
 // Moves (or segments) with fewer steps than this will be joined with the next move
-#define MIN_STEPS_PER_SEGMENT 6
+#define MIN_STEPS_PER_SEGMENT 1
 
 /**
  * Minimum delay before and after setting the stepper DIR (in ns)
